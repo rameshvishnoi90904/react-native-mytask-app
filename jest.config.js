ce5c08562824809e58c6da1d16bd0bc69d6f2cf0
rameshvishnoi90904 <rameshvishnoi90904@gmail.com>
2023-11-30 01:05:04 +0800

    feat: added testcases for app.tsx file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: [
+    "./jest/setup.js"
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|rollbar-react-native|@fortawesome|@react-native|@react-navigation)',
+  ],
 };
+
