7cc761c7914517e33191253cfdcf44ee057f3b7d
rameshvishnoi90904 <rameshvishnoi90904@gmail.com>
2023-11-30 00:51:54 +0800

    feat: added api layer

--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -1,7 +1,18 @@
+export type task = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export type dispatchActionType = {
+  type: string;
+  data: task | null;
+  searchInput: string;
+}
 
 export type NavigationScreenPropsType = {
-    Home: undefined;
-    TodoScreen: {}
-    LandingScreen: {};
-  };
-  
+  Home: undefined;
+  TodoScreen: {item: task | null };
+  LandingScreen: {};
+};
